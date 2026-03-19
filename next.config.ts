58653dd89b5ab5bccd6704fac6e3dfbf5363672e
Philip <philipos793@gmail.com>
2026-03-19 11:24:33 +0200

    remove next-pwa and use native next pwa setup

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
-import withPWA from "next-pwa"
+import type { NextConfig } from "next"
 
-const nextConfig = {
+const nextConfig: NextConfig = {
   reactStrictMode: true,
-  turbopack: {},
 }
 
-export default withPWA({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-})(nextConfig)
+export default nextConfig
